19496a28c2b1117ba230dc0a455c8a77caca2cc2
Alansiew <siewieraa@gmail.com>
2022-03-06 22:25:10 +0100

    changes

--- season_statistic.py
+++ season_statistic.py
@@ -17,7 +17,7 @@ listForSavingFile = defaultdict(list) # list for merge dictionaries
 
 def seasonStats(): # function get wanted data from api and create dictionaries with win, lose teams as a visitors and home teams
     season = input("Enter season year in YYYY format: ")
-
+    print("\nWaiting...")
     response_API = requests.get(
         'https://www.balldontlie.io/api/v1/games?seasons[]={}'.format(season) + '&per_page=100&page=1')
     python_data = json.loads(response_API.text)
@@ -85,7 +85,7 @@ ask=ask.lower()
 while True:
     if ask != "y":
         print("You chose to not save a file")
-        second_ask = input("Press enter for close script ")
+        second_ask = input("Press enter to exit script ")
         if not second_ask or second_ask:
             break
     if ask == "y":
